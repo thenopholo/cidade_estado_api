5dedddb61a0a24094d9e501794c448b38064a0d3
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-02-16 18:05:41 -0300

    FEAT: HTTP GET config done

--- internal/app/bootstrap/server.go
+++ internal/app/bootstrap/server.go
@@ -7,6 +7,7 @@ import (
 
 func StartServer() {
 	e := gin.Default()
+	configureRoutes(e)
 	err := e.Run(":8080")
 
 	if err != nil {
@@ -14,4 +15,15 @@ func StartServer() {
 	}
 
 	fmt.Println("Server Started")
+}
+
+func configureRoutes(e *gin.Engine) {
+	g := e.Group("/api/v1")
+	{
+		g.GET("/states", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"hello": "World",
+			})
+		})
+	}
 }
